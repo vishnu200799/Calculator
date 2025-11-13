1fad8602315073da79a164cd21c6568ace8627f4
vishnu200799 <vishnu80869@gmail.com>
2025-11-13 14:26:15 +0530

    Add calculator files

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendCharacter(char) {
+    document.getElementById('result').value += char;
+}
+
+function clearDisplay() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculateResult() {
+    let result = document.getElementById('result').value;
+    try {
+        document.getElementById('result').value = eval(result);
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
